bab527dce3ea16c183cfaa02b2d191dae3d32976
Ren Ichinose <kamatani.toru@gmail.com>
2023-02-28 04:36:44 +0900

    useQueryNotices（カスタムフック）の実装 #10

---
+++ frontend/hooks/useQueryNotices.ts
@@ -0,0 +1,23 @@
+import { useQuery } from 'react-query'
+import { Notice } from '../types/types'
+import { supabase } from '../utils/supabase'
+
+export const useQuerrNotices = () => {
+  const getNotices = async () => {
+    const { data, error } = await supabase
+      .from('notices')
+      .select('*')
+      .order('created_at', { ascending: true })
+
+    if (error) {
+      throw new Error(error.message)
+    }
+    return data
+  }
+  return useQuery<Notice[], Error>({
+    queryKey: ['notices'],
+    queryFn: getNotices,
+    staleTime: 0,
+    refetchOnWindowFocus: true,
+  })
+}
