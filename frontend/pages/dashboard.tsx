11953a894696bdb5b59e4064b221132bc032a61e
Ren Ichinose <kamatani.toru@gmail.com>
2023-03-03 23:59:29 +0900

    dashboard（ページ）の実装 #11

--- frontend/pages/dashboard.tsx
+++ frontend/pages/dashboard.tsx
@@ -1,12 +1,21 @@
-import { LogoutIcon } from '@heroicons/react/solid'
 import { NextPage } from 'next'
-import React from 'react'
-import Layout from '../components/Layout'
+import { useQueryClient } from 'react-query'
+import {
+  LogoutIcon,
+  StatusOnlineIcon,
+  DocumentTextIcon,
+} from '@heroicons/react/solid'
 import { supabase } from '../utils/supabase'
+import Layout from '../components/Layout'
+import { TaskList } from '../components/TaskList'
+import { TaskForm } from '../components/TaskForm'
 
 const Dashboard: NextPage = () => {
+  const queryClient = useQueryClient()
   const signOut = () => {
     supabase.auth.signOut()
+    queryClient.removeQueries('todos')
+    queryClient.removeQueries('notices')
   }
   return (
     <Layout title="Dashboard">
@@ -14,6 +23,15 @@ const Dashboard: NextPage = () => {
         className="mb-6 h-6 w-6 cursor-pointer text-blue-500"
         onClick={signOut}
       />
+      <div className="grid grid-cols-2 gap-40">
+        <div>
+          <div className="my-3 flex justify-center">
+            <DocumentTextIcon className=" h-8 w-8 text-blue-500" />
+          </div>
+          <TaskForm />
+          <TaskList />
+        </div>
+      </div>
     </Layout>
   )
 }
